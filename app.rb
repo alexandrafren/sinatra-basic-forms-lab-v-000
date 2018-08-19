2f928e87d64bcb9c6db08609d1593c53eb95ff4b
Alexandra Fren <alexandra.fren@gmail.com>
2018-08-19 23:57:34 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -10,7 +10,7 @@ class App < Sinatra::Base
     erb :create_puppy
   end
 
-  post '/display_puppy'
+  post '/display_puppy' do
     @newpuppy = Puppy.new(params[:name], params[:age], params[:breed])
     erb :display_puppy
   end
